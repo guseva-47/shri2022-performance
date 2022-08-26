9d8cd3a42714fb74c12b044a996c51f1a2b255b3
Aleksandra <guseva-47@yandex.ru>
2022-08-27 02:12:19 +0700

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/scripts.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
